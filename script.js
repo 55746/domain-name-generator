b8093d53af4cbadb36a9d81822c01db21621d1bb
55746 <Lawhead.gavin@live.jobcorps.org>
2022-03-14 18:05:13 +0000

    domain name generator

---
+++ script.js
@@ -0,0 +1,12 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+let ext = ['.com', '.net', '.io', '.ect']
+for(let i in pronoun){
+    for(let j in adj){
+        for(let k in noun){
+            for(let m in ext)
+                console.log(pronoun[1] + adj[j] + noun[k] + ext[m])
+        }
+    }
+}
